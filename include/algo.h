195abbf4ef8a94c9a0685f55d4d1a280c3b5c679
Gabe Geiser <gabe.geiser@gmail.com>
2022-10-17 17:21:04 -0500

    framework for further graph algorithms

---
+++ include/algo.h
@@ -0,0 +1,19 @@
+//
+// Created by 19012 on 10/17/2022.
+//
+
+#ifndef GRAPHS_ALGO_H
+#define GRAPHS_ALGO_H
+
+/**
+ * Graph algorithms
+ */
+
+#include "AdjList.h"
+
+
+//Checks to see if a given graph has a cycle in it
+bool hasCycle(const Graph& graph);
+
+
+#endif //GRAPHS_ALGO_H
---
+++ src/algo.cpp
@@ -0,0 +1,12 @@
+//
+// Created by 19012 on 10/17/2022.
+//
+
+
+#include "../include/algo.h"
+
+//TODO: Finish this implementation
+bool hasCycle(const Graph& graph) {
+    //get DFS visitor
+    return true;
+}
